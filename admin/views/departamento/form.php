d6cf70ed512a7d3124618cd6b636d44f4d3a2a16
AlejandraPG08 <102006570+AlejandraPG08@users.noreply.github.com>
2023-03-14 16:11:50 -0600

    Commits 5

--- admin/views/departamento/form.php
+++ admin/views/departamento/form.php
@@ -1,12 +1,12 @@
 <h1>Departamento</h1>
-<form method="POST" action="departamento.php?action=<?php echo $action; ?>">
+<form method="POST" action="departamento.php?action=<?php echo $accion; ?>">
     <div class="mb-3">
         <label  class="form-label">Nombre del Departamento</label>
         <input type="text" name="data[departamento]" class="form-control"  placeholder="Departamento" value="<?php echo isset($data[0]['departamento'])?$data[0]['departamento']:''; ?>">
     </div>
     <div class="mb-3">
         <?php 
-        if($action=='edit'): ?>
+        if($accion=='edit'): ?>
         <input type="hidden" name="data[id_departamento]" value="<?php echo isset($data[0]['departamento'])?$data[0]['departamento']:'';?>"
 
         <?php endif;?>
